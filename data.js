0ccc118d9ef8b1b9cb04e4d8b94a9c5d23a35414
Anshika Tiwari <khushitiwari5821@gmail.com>
2026-06-15 22:47:02 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -3,5 +3,10 @@
     "id": 1781462859609,
     "question": "What is the command to start this server?",
     "answer": "node server.js"
+  },
+  {
+    "id": 1781543427521,
+    "question": "what is 5+5",
+    "answer": "10"
   }
 ]
